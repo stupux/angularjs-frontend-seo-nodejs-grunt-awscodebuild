785abc6581c5fea105b944cfc809285d1d7c79c4
Marcus Maestri <marcus@stup.com.br>
2017-11-29 12:29:21 -0200

    buildspec

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000,
 http.createServer(function (req, res) {
   if (req.url.indexOf('/img') != -1) {
     var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
+    fs.readFile(__dirname + '/app/img' + filePath, function (err, data) {
       if (err) console.log(err);
       res.writeHead(200, {'Content-Type': 'image/svg+xml'});
       res.write(data);
@@ -13,7 +13,7 @@ http.createServer(function (req, res) {
     });
   } else if (req.url.indexOf('/js') != -1) {
     var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/public/js' + filePath, function (err, data) {
+    fs.readFile(__dirname + '/app/js' + filePath, function (err, data) {
       if (err) console.log(err);
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data);
@@ -21,14 +21,14 @@ http.createServer(function (req, res) {
     });
   } else if(req.url.indexOf('/css') != -1) {
     var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
+    fs.readFile(__dirname + '/app/css' + filePath, function (err, data) {
       if (err) console.log(err);
       res.writeHead(200, {'Content-Type': 'text/css'});
       res.write(data);
       res.end();
     });
   } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+    fs.readFile(__dirname + '/app/index.html', function (err, data) {
       if (err) console.log(err);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
